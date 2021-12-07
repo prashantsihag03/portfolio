e3f41da03ca7e0df4cdf25a7b8da33d455666e1b
prashantsihag03 <prashantsihag0309@gmail.com>
2021-12-08 00:04:00 +1030

    test

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,5 +1,9 @@
 import React from "react";
+import blogIcon from "../../resources/images/blog.svg";
 import webRtcImage from "../../resources/images/webRTC.png";
+import dynamoDBBlogImage from "../../resources/images/dynamoDBBlog.jpg";
+import facebookBlogImage from "../../resources/images/facebookBlog.png";
+import scientifcMethodForDataAnalysisImage from "../../resources/images/scientifcMethodForDataAnalysis.png";
 import '../css/Blogs.css';
 
 const Blogs = () => {
@@ -12,19 +16,19 @@ const Blogs = () => {
         },
         {
             link: "https://medium.com/analytics-vidhya/awss-dynamodb-and-how-does-it-work-357fde5995cb",
-            img: "../../resources/images/dynamoDBBlog.jpg",
+            img: dynamoDBBlogImage,
             heading: "AWS DynamoDB and How does it work?",
             desc: "In this blog, I have explored DynamoDB as a service, how does it work, and some important concepts to know before getting started with it.",
         },
         {
             link: "https://prashant-sihag.medium.com/getting-started-with-facebooks-graph-api-in-python-8189c37f7f3d",
-            img: "../../resources/images/facebookBlog.png",
+            img: facebookBlogImage,
             heading: "Getting started with Facebook's Graph API in Python",
             desc: "In this blog, I have explained how to get started with Facebook’s Graph API,which is a primary way of interacting with Facebook for developers, in a very simple and concise manner.",
         },
         {
             link: "https://medium.com/analytics-vidhya/scientific-method-for-data-analysis-41798626371a",
-            img: "../../resources/images/scientifcMethodForDataAnalysis.png",
+            img: scientifcMethodForDataAnalysisImage,
             heading: "Scientific Method for Data Analysis that can also be applied to other aspects of life.",
             desc: "In this blog, I have described in detail how scientific method for data analysis works and is utilised in data analysis which can also  be applied to other aspects of life for smoother planning and analysis.",
         },
@@ -34,7 +38,7 @@ const Blogs = () => {
         <section id="blogs">
             <div id="blogs-heading">
                 <h2>Blogs</h2>
-                <img src="../../resources/images/blog.svg" alt="" />
+                <img src={blogIcon} alt="" />
             </div>
             <ul>
                 {data.map((blog, id) => {
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ContactForm from "./ContactForm";
 import SectionHeading from "./SectionHeading";
+import blogIcon from "../../resources/images/blog.svg";
 import '../css/Contact.css';
 
 const Contact = () => {
     return (
         <section id="contact">
-            <SectionHeading heading="Get in touch..." icon="../../resources/images/blog.svg"/>
+            <SectionHeading heading="Get in touch..." icon={blogIcon}/>
             <ContactForm />
         </section>
     );
