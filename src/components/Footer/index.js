15b0b0fa067595a9510c3191f8e94c389914f501
Prashant Sihag <prashantsihag0309@gmail.com>
2022-02-18 20:50:17 +1030

    color change to white for footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,31 +1,29 @@
 import React from "react";
 import propTypes from "prop-types";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const StyledFooter = styled.footer`
-    display: block;
-    width: 100%;
-    margin: auto;
-    text-align: center;
-    padding: 0.1em;
-    color: white;
-    font-size: 1em;
-    font-weight: bold;
-    letter-spacing: 0.2em;
-    // background-color: var(--themeColor);
-    padding-bottom: 1.5em;
-    margin-top: 5vh;
-    border-top: 1px solid #383838;
+  display: block;
+  width: 100%;
+  margin: auto;
+  text-align: center;
+  padding: 0.1em;
+  color: white;
+  font-size: 1em;
+  font-weight: bold;
+  letter-spacing: 0.2em;
+  // background-color: var(--themeColor);
+  padding-bottom: 1.5em;
+  margin-top: 5vh;
+  border-top: 1px solid white;
 `;
 
 const Footer = (props) => {
-    return (
-        <StyledFooter> {props.content} </StyledFooter>
-    );
-}
+  return <StyledFooter> {props.content} </StyledFooter>;
+};
 
 Footer.propTypes = {
-    content: propTypes.string.isRequired,
-}
+  content: propTypes.string.isRequired,
+};
 
 export default Footer;
