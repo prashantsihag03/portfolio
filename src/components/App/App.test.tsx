2556033dece38e571f89c3f22e0a63ac88e7b351
Prashant Sihag <prashantsihag0309@gmail.com>
2022-07-10 23:50:59 +0930

    added new test for app

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,27 +1,11 @@
 import React from "react";
-import { render } from '@testing-library/react';
 import App from "./App";
+import { render } from '@testing-library/react';
 
-test('does NOT show scrollTo button when page first loads', () => {
-    const { findByTestId } = render(<App />);
-    expect(findByTestId("scrollToButton")).not.toBeInTheDocument();
-});
-
-test('does NOT show scrollTo button when page has been scrolled down to less than 300 px', () => {
-    const { container, findByTestId } = render(<App />);
-    container.scrollTo({
-        top: 299,
-        behavior: "smooth", // for smoothly scrolling
+describe("<App />", () => {
+    it('does not show scrollTo button when page first loads', () => {
+        const { queryByTestId } = render(<App />);
+        const scrollButton = queryByTestId("scrollToButton");
+        expect(scrollButton).not.toBeInTheDocument();
     });
-
-    expect(findByTestId("scrollToButton")).not.toBeInTheDocument();
-});
-
-test('shows scrollTo button when page has been scroll down 300 px', () => {
-    const { container, findByTestId } = render(<App />);
-    container.scrollTo({
-        top: 301,
-        behavior: "smooth", // for smoothly scrolling
-    });
-    expect(findByTestId("scrollToButton")).toBeInTheDocument();
-});
+})
