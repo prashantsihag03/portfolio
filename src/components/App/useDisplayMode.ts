e907a85dcd2a7299ee18cdb438b21c8b6b59da65
Prashant Sihag <prashantsihag0309@gmail.com>
2024-01-20 00:08:37 +1030

    - updating theme-color meta tag content based on display preference

--- src/components/App/useDisplayMode.ts
+++ src/components/App/useDisplayMode.ts
@@ -4,32 +4,39 @@ import getDesignTokens from "../../Theme";
 import { DisplayMode } from "../../types";
 
 const useDisplayMode = () => {
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-  const [displayMode, setDisplayMode] = React.useState<DisplayMode>(prefersDarkMode ? "dark" : "light");
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const [displayMode, setDisplayMode] = React.useState<DisplayMode>(
+    prefersDarkMode ? "dark" : "light"
+  );
 
   const toggleDisplayMode = () => {
-    if (displayMode==="light") {
+    if (displayMode === "light") {
       const metaThemeColor = document.querySelector("meta[name=theme-color]");
       if (metaThemeColor) metaThemeColor.setAttribute("content", "#000000");
       setDisplayMode("dark");
-    }
-    else {
+    } else {
       const metaThemeColor = document.querySelector("meta[name=theme-color]");
       if (metaThemeColor) metaThemeColor.setAttribute("content", "#FFF");
       setDisplayMode("light");
     }
-  }
+  };
+
+  React.useEffect(() => {
+    document
+      .querySelector('meta[name="theme-color"]')
+      ?.setAttribute("content", displayMode === "dark" ? "#000000" : "#fff");
+  }, [displayMode]);
 
   const theme = React.useMemo(
-    () => createTheme(getDesignTokens(displayMode)), 
+    () => createTheme(getDesignTokens(displayMode)),
     [displayMode]
   );
-  
+
   return {
     theme,
     displayMode,
     toggleDisplayMode,
-  }
-}
+  };
+};
 
 export default useDisplayMode;
