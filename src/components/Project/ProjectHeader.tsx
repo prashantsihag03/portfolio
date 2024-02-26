4fef678d23f480ded496eac1354985985e506d67
Prashant Sihag <prashantsihag0309@gmail.com>
2024-02-27 03:20:06 +1030

    temporary hiding tour video button

--- src/components/Project/ProjectHeader.tsx
+++ src/components/Project/ProjectHeader.tsx
@@ -27,9 +27,9 @@ const ProjectHeader: React.FC = () => {
         alignItems={'center'}
         paddingTop={isMobile ? '1rem' : 0}
       >
-        <Button variant="outlined" color="primary">
+        {/* <Button variant="outlined" color="primary">
           <Typography variant="body2">Tour Video</Typography>
-        </Button>
+        </Button> */}
         <GithubIcon href={PROJECT_DIALOGUE.github} size="1.5rem" />
         <Icon
           size="2.2rem"
