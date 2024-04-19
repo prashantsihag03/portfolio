0c9be3bda91277cd3ce0f6ed1aa66f7b3aa42cf0
Prashant Sihag <prashantsihag0309@gmail.com>
2024-04-19 20:19:50 +0930

    Added better navigation between project media categories, fixed project media height to 50vh on mobile

--- src/components/Project/ProjectMediaBox.tsx
+++ src/components/Project/ProjectMediaBox.tsx
@@ -1,10 +1,11 @@
-import { Box, Button, Stack } from '@mui/material'
+import { Box, Stack } from '@mui/material'
 import FullscreenIcon from '@mui/icons-material/Fullscreen'
 import FullscreenExitIcon from '@mui/icons-material/FullscreenExit'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack'
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
 import { useState } from 'react'
 import { ProjectImgMap } from '../../Data/Projects'
+import MediaSwitcher, { MediaCategory } from './MediaSwitcher'
 
 interface ProjectMediaBoxProps {
   productImgUrls: ProjectImgMap[]
@@ -19,6 +20,8 @@ const ProjectMediaBox: React.FC<ProjectMediaBoxProps> = ({
 }: ProjectMediaBoxProps) => {
   const [isFullScreen, setIsFullScreen] = useState<boolean>(false)
   const [activeImgIndex, setActiveImgIndex] = useState<number>(0)
+  const [mediaCategory, setMediaCategory] = useState<MediaCategory>('product')
+
   const [totalImages] = useState<number>(
     productImgUrls.length +
       architectureDiagramUrls.length +
@@ -54,18 +57,58 @@ const ProjectMediaBox: React.FC<ProjectMediaBoxProps> = ({
     }
   }
 
+  const mediaCategoryByImgIndex = (index: number): MediaCategory => {
+    if (index + 1 <= productImgUrls.length) {
+      return 'product'
+    }
+    if (index + 1 <= productImgUrls.length + architectureDiagramUrls.length) {
+      return 'architecture'
+    }
+    if (
+      index + 1 <=
+      productImgUrls.length +
+        architectureDiagramUrls.length +
+        demoVideoUrls.length
+    ) {
+      return 'demo'
+    }
+    return 'product'
+  }
+
   const nextMedia = () => {
     if (activeImgIndex + 1 === totalImages) {
       setActiveImgIndex(0)
+      setMediaCategory('product')
     } else {
-      console.log('setting img to index', activeImgIndex + 1)
       setActiveImgIndex(activeImgIndex + 1)
+      const mediaCategory = mediaCategoryByImgIndex(activeImgIndex + 1)
+      setMediaCategory(mediaCategory)
     }
   }
 
   const prevMedia = () => {
-    if (activeImgIndex === 0) setActiveImgIndex(totalImages - 1)
-    else setActiveImgIndex(activeImgIndex - 1)
+    if (activeImgIndex === 0) {
+      setActiveImgIndex(totalImages - 1)
+      setMediaCategory(mediaCategoryByImgIndex(totalImages - 1))
+    } else {
+      setActiveImgIndex(activeImgIndex - 1)
+      setMediaCategory(mediaCategoryByImgIndex(activeImgIndex - 1))
+    }
+  }
+
+  const mediaSwitchClickHandler = (clickedSwitch: MediaCategory) => {
+    if (clickedSwitch === 'product') {
+      setMediaCategory('product')
+      setActiveImgIndex(0)
+    }
+    if (clickedSwitch === 'architecture') {
+      setMediaCategory('architecture')
+      setActiveImgIndex(productImgUrls.length)
+    }
+    if (clickedSwitch === 'demo') {
+      setMediaCategory('demo')
+      setActiveImgIndex(architectureDiagramUrls.length)
+    }
   }
 
   return (
@@ -81,8 +124,7 @@ const ProjectMediaBox: React.FC<ProjectMediaBoxProps> = ({
         component={'img'}
         src={getActiveImg()}
         width={'100%'}
-        height={'100%'}
-        maxHeight={'50vh'}
+        height={'40vh'}
         sx={{ objectFit: 'contain' }}
       />
       <Stack
@@ -91,6 +133,7 @@ const ProjectMediaBox: React.FC<ProjectMediaBoxProps> = ({
         justifyContent={'space-around'}
         alignItems={'center'}
         paddingBottom={'1rem'}
+        paddingTop={'1rem'}
         columnGap={1}
       >
         {totalImages > 1 ? <ArrowBackIcon onClick={prevMedia} /> : null}
@@ -115,7 +158,11 @@ const ProjectMediaBox: React.FC<ProjectMediaBoxProps> = ({
               src={getActiveImg()}
               width={'80%'}
               height={'80%'}
-              sx={{ objectFit: 'contain' }}
+              maxHeight={'80dvh'}
+              sx={{
+                objectFit: 'contain',
+                paddingTop: '1rem',
+              }}
             />
             <Stack
               direction={'row'}
@@ -123,6 +170,7 @@ const ProjectMediaBox: React.FC<ProjectMediaBoxProps> = ({
               justifyContent={'space-around'}
               alignItems={'center'}
               paddingTop={'1rem'}
+              paddingBottom={'1rem'}
             >
               {totalImages > 1 ? <ArrowBackIcon onClick={prevMedia} /> : null}
               <FullscreenExitIcon
@@ -134,55 +182,24 @@ const ProjectMediaBox: React.FC<ProjectMediaBoxProps> = ({
                 <ArrowForwardIcon onClick={nextMedia} />
               ) : null}
             </Stack>
+            <MediaSwitcher
+              activeMedia={mediaCategory}
+              architectureDiagramsCount={architectureDiagramUrls.length}
+              demoVideosCount={demoVideoUrls.length}
+              productImgsCount={productImgUrls.length}
+              onClick={mediaSwitchClickHandler}
+            />
           </Stack>
         ) : null}
         {totalImages > 1 ? <ArrowForwardIcon onClick={nextMedia} /> : null}
       </Stack>
-      <Stack
-        direction={'row'}
-        justifyContent={'center'}
-        alignItems={'center'}
-        paddingBottom={'3rem'}
-        columnGap={1}
-      >
-        {productImgUrls.length > 0 ? (
-          <Button
-            variant="text"
-            sx={{ fontSize: '0.7rem' }}
-            onClick={() => {
-              if (productImgUrls.length > 0) {
-                setActiveImgIndex(0)
-              }
-            }}
-          >
-            App Images ({productImgUrls.length})
-          </Button>
-        ) : null}
-        {architectureDiagramUrls.length > 0 ? (
-          <Button
-            variant="text"
-            sx={{ fontSize: '0.7rem' }}
-            onClick={() => {
-              if (architectureDiagramUrls.length > 0)
-                setActiveImgIndex(productImgUrls.length)
-            }}
-          >
-            Architecture Diagrams ({architectureDiagramUrls.length})
-          </Button>
-        ) : null}
-        {demoVideoUrls.length > 0 ? (
-          <Button
-            variant="text"
-            sx={{ fontSize: '0.7rem' }}
-            onClick={() => {
-              if (demoVideoUrls.length > 0)
-                setActiveImgIndex(architectureDiagramUrls.length)
-            }}
-          >
-            Demo Videos ({demoVideoUrls.length})
-          </Button>
-        ) : null}
-      </Stack>
+      <MediaSwitcher
+        activeMedia={mediaCategory}
+        architectureDiagramsCount={architectureDiagramUrls.length}
+        demoVideosCount={demoVideoUrls.length}
+        productImgsCount={productImgUrls.length}
+        onClick={mediaSwitchClickHandler}
+      />
     </Stack>
   )
 }
